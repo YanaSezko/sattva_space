94d11309e2faea59a2b3187093f20a6a1b2a2f1e
unknown <sezko.yana@gmail.com>
2025-04-01 11:11:45 +0300

    edit

--- src/data/musicData.js
+++ src/data/musicData.js
@@ -2,16 +2,25 @@ const BASE_URL = "https://yanasezko.github.io/sattva_space";
 
 export const albumsData = {
   "Аффирмации": [
-    { title: "Песня 1", src: `${BASE_URL}/music/song1.mp3`, type: "audio/mpeg" },
-    { title: "Песня 2", src: `${BASE_URL}/music/song2.mp3`, type: "audio/mpeg" },
-    { title: "Песня 3", src: `${BASE_URL}/music/song3.mp3`, type: "audio/mpeg" }
+    { title: "Настройка на день", src: `${BASE_URL}/music/affirm.mp3`, type: "audio/mpeg" },
+    { title: "Маяк", src: `${BASE_URL}/music/mayak.mp3`, type: "audio/mpeg" },
+    { title: "Волна", src: `${BASE_URL}/music/volna.mp3`, type: "audio/mpeg" },
+    { title: "Код", src: `${BASE_URL}/music/kod.mp3`, type: "audio/mpeg" },
+    { title: "Комета", src: `${BASE_URL}/music/kometa.mp3`, type: "audio/mpeg" },
+    { title: "Свет", src: `${BASE_URL}/music/svet.mp3`, type: "audio/mpeg" },
+    { title: "Отражение", src: `${BASE_URL}/music/otrajenie.mp3`, type: "audio/mpeg" },
+    { title: "Принятие себя", src: `${BASE_URL}/music/prinyatieseba.mp3`, type: "audio/mpeg" },
+    { title: "Радио", src: `${BASE_URL}/music/radio.mp3`, type: "audio/mpeg" },
+    { title: "В моєму серці", src: `${BASE_URL}/music/vmoemuserci.mp3`, type: "audio/mpeg" },
+    { title: "Бачу ціль", src: `${BASE_URL}/music/bachucil.mp3`, type: "audio/mpeg" },
+  ],
+  "Песни": [
+    { title: "Люди", src: `${BASE_URL}/music/ludi.mp3`, type: "audio/mpeg" },
+    { title: "Обиды", src: `${BASE_URL}/music/obidu.mp3`, type: "audio/mpeg" },
+    { title: "Справедливость", src: `${BASE_URL}/music/spravedlivost.mp3`, type: "audio/mpeg" }
   ],
   "Медитации": [
     { title: "1", src: `${BASE_URL}/music/song4.mp3`, type: "audio/mpeg" },
     { title: "2", src: `${BASE_URL}/music/song5.mp3`, type: "audio/mpeg" }
-  ],
-  "Песни": [
-    { title: "1", src: `${BASE_URL}/music/song6.mp3`, type: "audio/mpeg" },
-    { title: "2", src: `${BASE_URL}/music/song7.mp3`, type: "audio/mpeg" }
   ]
 };
